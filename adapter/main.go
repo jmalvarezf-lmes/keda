5db83e0870cdfd594d39f35a31471517e78dd6d7
Ahmed ElSayed <ahmels@microsoft.com>
2021-10-14 16:27:34 -0700

    Update adapter/main.go
    Co-authored-by: Zbynek Roubalik <726523+zroubalik@users.noreply.github.com>

--- adapter/main.go
+++ adapter/main.go
@@ -27,7 +27,6 @@ import (
 
 	kedacontrollers "github.com/kedacore/keda/v2/controllers/keda"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
